c32bad60e0faab74de2924d2853313f3bf61c066
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2024-01-17 20:15:39 +0000

    Initial commit

---
+++ OTS.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class OTS here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class OTS
+{
+    // instance variables - replace the example below with your own
+    
+    public boolean isLoggedIn;
+
+    /**
+     * Constructor for objects of class OTS
+     */
+    public OTS()
+    {
+        isLoggedIn = false;
+        
+        
+    }
+    
+    public void SignUp(){
+        //do sth;
+
+
+    }
+    
+    public void LogIn(){
+        
+    }
+    
+    public void SeeTickets(){
+        
+    }
+    
+    public void CancelTickets(){
+    
+    
+    }
+    
+    public void SeeEvents(){
+    
+    }
+    
+    
+}
